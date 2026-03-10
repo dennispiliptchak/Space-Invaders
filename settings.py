087e5ab9935a41eafb456e5d3932ed88484f2c5c
dennispiliptchak <dennisp3@umbc.edu>
2026-03-09 20:33:53 -0400

    implement ship movement

--- settings.py
+++ settings.py
@@ -3,7 +3,11 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
+
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Ship Settings
+        self.ship_speed = 1.5
